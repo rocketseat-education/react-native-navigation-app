ce5af7f7a570732643913e818beda2defd3cf114
arthur <arthur.rios007@gmail.com>
2025-03-26 13:41:06 -0300

    Navegação - Tipagem flexível

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,15 +1,11 @@
 import { View } from 'react-native'
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
-
-import { StackRoutesList } from '@/routes/StackRoutes'
 
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
 import { ButtonIcon } from '@/components/ButtonIcon'
+import { StackRoutesProps } from '@/routes/StackRoutes'
 
-type Props = NativeStackScreenProps<StackRoutesList, 'home'>
-
-export function Home({ navigation }: Props) {
+export function Home({ navigation }: StackRoutesProps<'home'>) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
--- src/routes/StackRoutes.tsx
+++ src/routes/StackRoutes.tsx
@@ -1,4 +1,7 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack'
 
 import { Home } from '@/app/Home'
 import { Product } from '@/app/Product'
@@ -7,6 +10,9 @@ export type StackRoutesList = {
   home: undefined
   product: undefined
 }
+
+export type StackRoutesProps<T extends keyof StackRoutesList> =
+  NativeStackScreenProps<StackRoutesList, T>
 
 const Stack = createNativeStackNavigator<StackRoutesList>()
 
