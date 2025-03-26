fa73cd294e60865b6714627e1ecf031975f6688f
arthur <arthur.rios007@gmail.com>
2025-03-26 13:37:53 -0300

    Navegação - Utilizando prop de navegação

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,13 +1,15 @@
 import { View } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import { StackRoutesList } from '@/routes/StackRoutes'
 
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
 import { ButtonIcon } from '@/components/ButtonIcon'
 
-export function Home() {
-  const navigation = useNavigation()
+type Props = NativeStackScreenProps<StackRoutesList, 'home'>
 
+export function Home({ navigation }: Props) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,13 +1,15 @@
 import { View } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+
+import { StackRoutesList } from '@/routes/StackRoutes'
 
 import { Header } from '@/components/Header'
 import { Title } from '@/components/Title'
 import { ButtonIcon } from '@/components/ButtonIcon'
 
-export function Product() {
-  const navigation = useNavigation()
+type Props = NativeStackScreenProps<StackRoutesList, 'product'>
 
+export function Product({ navigation }: Props) {
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
