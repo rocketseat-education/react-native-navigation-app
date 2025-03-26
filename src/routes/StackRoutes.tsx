0b3908eb2873ca7782f4b8806252b45ad4a71c6c
arthur <arthur.rios007@gmail.com>
2025-03-26 13:22:09 -0300

    Navegação - Header

--- src/routes/StackRoutes.tsx
+++ src/routes/StackRoutes.tsx
@@ -7,7 +7,10 @@ const Stack = createNativeStackNavigator()
 
 export function StackRoutes() {
   return (
-    <Stack.Navigator initialRouteName="home">
+    <Stack.Navigator
+      initialRouteName="home"
+      screenOptions={{ headerShown: false }}
+    >
       <Stack.Screen name="home" component={Home} />
       <Stack.Screen name="product" component={Product} />
     </Stack.Navigator>
