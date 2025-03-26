0c6dd1d4083afb7960a04bf07d782525594dbf01
arthur <arthur.rios007@gmail.com>
2025-03-26 12:55:33 -0300

    Navegação - Criando Home

--- App.tsx
+++ App.tsx
@@ -1,21 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from '@/app/Home'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello, World!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Home />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
